f4a7ddf9b8612a54373ee7b0ca82c43f0c981697
MichaelScofield45 <paco.villamonte@gmail.com>
2023-10-08 22:07:00 -0600

    Cross platform dev env

--- flake.nix
+++ flake.nix
@@ -1,22 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, ... }: 
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    lib = pkgs.lib;
-  in
-  {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        xorg.libX11
-        libGL
-      ];
+  outputs = { nixpkgs, flake-utils, ... }: 
+  flake-utils.lib.eachDefaultSystem (system: 
+    let
+      pkgs = import nixpkgs { inherit system; };
+      lib = pkgs.lib;
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          xorg.libX11
+          libGL
+        ];
 
-      LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs;[ xorg.libX11 libGL ]);
-    };
-  };
-}
+        LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs;[ xorg.libX11 libGL ]);
+      };
+    });
+  }
